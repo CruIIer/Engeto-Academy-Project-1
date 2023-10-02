ae2d58eea9993dff6cd4d3966142b63773530044
Cruller <apreclik@centrum.cz>
2023-10-02 19:07:28 +0200

    Vytvoření projektu a zapsání základních bodů

---
+++ Projekt_1.py
@@ -0,0 +1,37 @@
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+
+autor: Aleš Preclík
+email: apreclik@centrum.cz
+discord: alda_p
+"""
+
+user = input("username: ")
+password = input("password: ")
+if user == "bob" and password == "123":
+    print("Welcome to the app", user)
+elif user == "ann" and password == "pass123":
+    print("Welcome to the app", user)
+elif user == "mike" and password == "password123":
+    print("Welcome to the app", user)
+elif user == "liz" and password == "pass123":
+    print("Welcome to the app", user)
+else:
+    print("unregistered user, terminating the program")
+
+print("We have 3 texts to be analyzed.")
+
+vyber = int(input("Enter a number btw. 1 and 3 to select:"))
+if vyber < 1 or vyber > 3:
+    print("Chosen number is not in the selection")
+elif vyber != type(int):
+    print("You must select a number")
+
+
+# ---------------
+
+# 6. pro vybraný text spočítá následující statistiky 
+# 7. 
+
+# ---------------
+
